2e05c4a7c8b85b076c785612e47726e2dd7b52ab
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-22 00:18:00 -0300

    Evento de clique ocorre apenas no primeiro clique e criada a fução de torca de jogador

--- script/script.js
+++ script/script.js
@@ -1,3 +1,19 @@
 //Iniviar variáveis globais do jogo
 const simbolos = ["X","O"]
 let jogadorAtual = 0
+const jogadasTabuleiro = ["","","","","","","","",""]
+
+const celulas = document.querySelectorAll(".celula") //Selecionar todas as celulas do tabuleiro
+
+celulas.forEach(celula => {
+    celula.addEventListener("click", fazerJogada, {once: true})
+})
+
+function fazerJogada(celula){
+    celula.target.innerHTML = simbolos[jogadorAtual]
+    trocarJogador()
+}
+
+function trocarJogador(){
+    jogadorAtual = jogadorAtual == 0 ? 1 : 0 //Operador ternario, se jogadorAtual for 0 ele será trocado por 1
+}
